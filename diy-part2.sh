e0443ce77c71206749dd6373bf3c232bcdba5700
huhaha0105 <130190944+huhaha0105@users.noreply.github.com>
2026-03-22 16:27:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# 默认IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# IP地址
+sed -i 's/192.168.1.1/192.168.5.100/g' package/base-files/files/bin/config_generate
 
 # 主机名
-sed -i 's/OpenWrt/TR3000-Pro/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/TR3000/g' package/base-files/files/bin/config_generate
 
 # 默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# iStoreOS 伪装
+# iStoreOS伪装
 sed -i 's/OpenWrt/iStoreOS/g' package/base-files/files/etc/openwrt_release
 
 # Rust修复
 sed -i 's/ci-llvm=true/ci-llvm=false/g' feeds/packages/lang/rust/Makefile
 
-# OpenClash内核
+# OpenClash Meta
 mkdir -p files/etc/openclash/core
 wget -qO clash.tar.gz https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz
 tar -zxvf clash.tar.gz -C files/etc/openclash/core/
